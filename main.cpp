d78bc3758aea7ac4cea23f65ed5dbc6a95bd0011
Isaac <isaacatkinson04@gmail.com>
2025-08-26 03:50:08 +0100

    Changed function balance to showBalance
    Added switch statement to take the users choice of action

--- main.cpp
+++ main.cpp
@@ -1,23 +1,66 @@
 #include <iostream>
 using namespace std;
 
-
-// show balance, add money, remove money
-
-double balance();
+// Declare the functions
+double showBalance(double balance);
 void deposit();
-void withdraw();
+void withdraw(double balance);
 
 int main() {
 
-    balance();
+    // Initialise the users balance
+    double balance = 0;
+
+    // Initialise the users choice of show balance, deposit money or withdraw money
+    int choice = 0;
+
+    // Ask the user with a list of options what they want to do
+    cout << "What do You want to do: \n\n";
+
+    cout << "1. Display your Balance\n";
+    cout << "2. Deposit Money\n";
+    cout << "3. Withdraw Money\n";
+    cout << "4. Exit\n\n";
+
+    // Get the users choice
+    cin >> choice;
+
+    // Switch statement to take the users choice and perform the correct function
+    switch (choice) {
+
+        case 1:
+            cout << "You have a balance of : £";
+            showBalance(balance);
+            break;
+
+        case 2:
+            cout << "You want to deposit Money\n";
+            deposit();
+            break;
+
+        case 3:
+            cout << "You want to withdraw Money\n";
+            withdraw(balance);
+            break;
+
+        case 4:
+            cout << "Goodbye?";
+
+        default:
+            cout << "Please Select 1-4!";
+
+    }
 
 }
 
-double balance(double amount) {
+// Function to show the balance
+double showBalance(double amount) {
 
+    cout << "You have a total of : \n  £";
+    return amount;
 }
 
+// Function to deposit money
 void deposit() {
 
     double deposit = 0;
@@ -26,7 +69,8 @@ void deposit() {
     cin >> deposit;
 }
 
-void withdraw() {
+// Function to withdraw money
+void withdraw(double balance) {
 
     double withdraw = 0;
 
