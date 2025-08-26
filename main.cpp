8af28c0a75c7c9abfeee8bdf29033112848c4158
Isaac <isaacatkinson04@gmail.com>
2025-08-26 03:16:49 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+
+// show balance, add money, remove money
+
+double balance();
+void deposit();
+void withdraw();
+
+int main() {
+
+    balance();
+
+}
+
+double balance(double amount) {
+
+}
+
+void deposit() {
+
+    double deposit = 0;
+
+    cout << "Enter the amount to be deposited: \n";
+    cin >> deposit;
+}
+
+void withdraw() {
+
+    double withdraw = 0;
+
+    cout << "Enter the amount to be withdrawn: \n";
+    cin >> withdraw;
+}
